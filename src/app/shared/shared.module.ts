e2b9e905b7102bb82a72a9dfff886c0694d2d2d2
GonzaloLatorre <gonza.latorre21@gmail.com>
2023-06-14 11:26:43 -0300

    creamos componente navbar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavbarComponent
+  ],
   imports: [
     CommonModule
   ]
